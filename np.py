1ce010167d06921bedb539405e7989457d59ef81
Axisevil <shahaleem8838@gmail.com>
2021-10-11 19:18:47 +0530

    complete files

---
+++ np.py
@@ -0,0 +1,12 @@
+import time
+from plyer import notification
+
+if__name__ ="__main__"
+while True:
+    notification.notify(
+    title='Please Take A Quick Nap',
+    message="it imporoves your work rate",
+    app_icon='E:\\python projects\\New folder\\icon.ico',
+    timeout=10
+
+    )
